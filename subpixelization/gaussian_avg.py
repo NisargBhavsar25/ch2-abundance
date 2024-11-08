dfcf6bd0c09de74f9e4ce6d0dc6855eb4b8ea73b
yashkumar2603 <yash.2603kumar@gmail.com>
2024-11-09 03:08:04 +0530

    Updated to use convexhull for handling random quadrilateral shape

--- subpixelization/gaussian_avg.py
+++ subpixelization/gaussian_avg.py
@@ -1,10 +1,8 @@
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
+from scipy.spatial import ConvexHull
+from skimage.draw import polygon
 
 class GaussianArray:
     def __init__(self, grid_size=64):
-        # Initialize a 3D array with a data layer and a count layer
         self.arr = np.zeros((grid_size, grid_size, 2))
         self.grid_size = grid_size
 
@@ -18,10 +16,8 @@ class GaussianArray:
         lat_scale = (self.grid_size - 1) / (block_lat[2] - block_lat[0])
         lon_scale = (self.grid_size - 1) / (block_lon[2] - block_lon[0])
 
-        lat_indices = [(lat[0] - block_lat[0]) * lat_scale, (lat[1] - block_lat[0]) * lat_scale,
-                       (lat[2] - block_lat[0]) * lat_scale, (lat[3] - block_lat[0]) * lat_scale]
-        lon_indices = [(lon[0] - block_lon[0]) * lon_scale, (lon[1] - block_lon[0]) * lon_scale,
-                       (lon[2] - block_lon[0]) * lon_scale, (lon[3] - block_lon[0]) * lon_scale]
+        lat_indices = [(lat[i] - block_lat[0]) * lat_scale for i in range(4)]
+        lon_indices = [(lon[i] - block_lon[0]) * lon_scale for i in range(4)]
 
         lat_indices = list(map(int, lat_indices))
         lon_indices = list(map(int, lon_indices))
@@ -32,29 +28,31 @@ class GaussianArray:
         x = np.arange(0, shape[0], 1, float)
         y = np.arange(0, shape[1], 1, float)
         x, y = np.meshgrid(x, y)
-        gauss = (np.exp(-((x - center[0]) ** 2 + (y - center[1]) ** 2) / (2 * sigma ** 2)))/(2 * np.pi * sigma ** 2)
-        return gauss / np.max(gauss)  # Normalize to max value of 1
+        gauss = (np.exp(-((x - center[0]) ** 2 + (y - center[1]) ** 2) / (2 * sigma ** 2))) / (2 * np.pi * sigma ** 2)
+        return gauss / np.max(gauss)
 
     def fill_up_the_array(self, img_lat, img_lon, block_lat, block_lon, max_value, sigma=0):
         if self.in_block_or_not(img_lat, img_lon, block_lat, block_lon):
             img_lat_indices, img_lon_indices = self.convert_coords_to_indices(img_lat, img_lon, block_lat, block_lon)
+            poly_points = np.array([img_lat_indices, img_lon_indices]).T
+            min_y, min_x = np.min(poly_points, axis=0)
+            max_y, max_x = np.max(poly_points, axis=0)
 
-            height = img_lat_indices[2] - img_lat_indices[0] + 1
-            width = img_lon_indices[2] - img_lon_indices[0] + 1
-            center = (height // 2, width // 2)
+            height, width = max_y - min_y + 1, max_x - min_x + 1
+            gaussian_values = self.generate_gaussian_distribution((height, width), (height // 2, width // 2), sigma) * max_value
 
-            gaussian_values = self.generate_gaussian_distribution((height, width), center, sigma) * max_value
+            # Mask the Gaussian to only fit inside the quadrilateral
+            rr, cc = polygon(poly_points[:, 0] - min_y, poly_points[:, 1] - min_x, gaussian_values.shape)
 
-            for i, x in enumerate(range(img_lon_indices[0], img_lon_indices[2] + 1)):
-                for j, y in enumerate(range(img_lat_indices[0], img_lat_indices[2] + 1)):
-                    value = gaussian_values[j, i]
-                    if self.arr[x, y, 1] == 0:  # If this cell hasn't been assigned a value yet
-                        self.arr[x, y, 0] = value
-                        self.arr[x, y, 1] = 1
-                    else:
-                        count = self.arr[x, y, 1]
-                        self.arr[x, y, 0] = (self.arr[x, y, 0] * count + value) / (count + 1)
-                        self.arr[x, y, 1] += 1
+            for r, c in zip(rr, cc):
+                x, y = r + min_y, c + min_x
+                if self.arr[x, y, 1] == 0:  # If cell hasn't been assigned a value yet
+                    self.arr[x, y, 0] = gaussian_values[r, c]
+                    self.arr[x, y, 1] = 1
+                else:
+                    count = self.arr[x, y, 1]
+                    self.arr[x, y, 0] = (self.arr[x, y, 0] * count + gaussian_values[r, c]) / (count + 1)
+                    self.arr[x, y, 1] += 1
 
     def add_gaussian_box(self, img_lat, img_lon, block_lat, block_lon, max_value, sigma=0, plot=False):
         self.fill_up_the_array(img_lat, img_lon, block_lat, block_lon, max_value, sigma)
@@ -62,7 +60,7 @@ class GaussianArray:
             self.visualize_heatmap()
 
     def visualize_heatmap(self):
-        heatmap_data = self.arr[:, :, 0]  # Select the first layer for the heatmap
+        heatmap_data = self.arr[:, :, 0]
         plt.figure(figsize=(10, 8))
         sns.heatmap(heatmap_data, cmap='viridis')
         plt.title('Heatmap of arr Layer 0 with Gaussian Distributions')
@@ -72,10 +70,8 @@ class GaussianArray:
 
 # Example usage
 gaussian_array = GaussianArray()
-
-# Adding multiple overlapping boxes with varying parameters
-gaussian_array.add_gaussian_box([-20, 20, 20, -20], [-20, -20, 20, 20], [-100, 100, 100, -100], [-100, -100, 100, 100], 1, sigma=5, plot=True)
-gaussian_array.add_gaussian_box([0, 40, 40, 0], [0, 0, 40, 40], [-100, 100, 100, -100], [-100, -100, 100, 100], 3, sigma=9, plot=True)
-gaussian_array.add_gaussian_box([-10, 30, 30, -10], [-10, -10, 30, 30], [-100, 100, 100, -100], [-100, -100, 100, 100], 2, sigma=3, plot=True)
-gaussian_array.add_gaussian_box([10, 50, 50, 10], [10, 10, 50, 50], [-100, 100, 100, -100], [-100, -100, 100, 100], 4, sigma=6, plot=True)
-gaussian_array.add_gaussian_box([-30, 10, 10, -30], [-30, -30, 10, 10], [-100, 100, 100, -100], [-100, -100, 100, 100], 1.5, sigma=12, plot=True)
+gaussian_array.add_gaussian_box([-20, 20, 25, -30], [-20, -10, 30, 25], [-100, 100, 100, -100], [-100, -100, 100, 100], 1, sigma=5, plot=True)
+gaussian_array.add_gaussian_box([0, 60, 40, 0], [0, 0, 40, 80], [-100, 100, 70, -100], [-100, -100, 100, 80], 3, sigma=9, plot=True)
+gaussian_array.add_gaussian_box([-10, 40, 30, -10], [-25, -10, 30, 30], [-100, 100, 140, -100], [-100, -120, 100, 100], 2, sigma=3, plot=True)
+gaussian_array.add_gaussian_box([10, 50, 60, 10], [10, 10, 50, 80], [-100, 100, 130, -100], [-100, -100, 100, 150], 4, sigma=6, plot=True)
+gaussian_array.add_gaussian_box([-30, 10, 20, -30], [-30, -50, 10, 10], [-100, 100, 120, -100], [-100, -110, 100, 100], 1.5, sigma=12, plot=True)
